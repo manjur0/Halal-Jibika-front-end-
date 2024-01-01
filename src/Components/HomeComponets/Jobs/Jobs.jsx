8b27fcf8e655107644971587a6a2c4e5f7130646
manjur0 <sfmanjur21@gmail.com>
2024-01-02 00:33:27 +0600

    Json data some configaration

--- src/Components/HomeComponets/Jobs/Jobs.jsx
+++ src/Components/HomeComponets/Jobs/Jobs.jsx
@@ -2,20 +2,27 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Job from "./Job";
 
-const Jobs = ({slices}) => {
+const Jobs = ({ slices }) => {
   const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
     const fetchJobs = async () => {
-      const response = await fetch("../../../../public/Jobs.json");
-      const data = await response.json();
-      setJobs(data);
+      try {
+        const response = await fetch("/Jobs.json");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setJobs(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
     fetchJobs();
   }, []);
   return (
     <div className="mx-auto max-w-7xl my-20   ">
-      <div   className=" grid grid-cols-1 mx-auto  gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div className=" grid grid-cols-1 mx-auto  gap-4 md:grid-cols-2 lg:grid-cols-3">
         {jobs.slice(0, slices).map((job) => (
           <Job key={job.id} job={job} />
         ))}
