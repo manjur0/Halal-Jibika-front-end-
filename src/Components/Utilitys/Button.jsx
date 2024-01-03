b12f8d203194f349fb16e1931b9f1697f1281f4b
manjur0 <sfmanjur21@gmail.com>
2024-01-03 18:26:22 +0600

    New job  post form added

--- src/Components/Utilitys/Button.jsx
+++ src/Components/Utilitys/Button.jsx
@@ -6,7 +6,7 @@ const Button = ({ seeAll, route , icons }) => {
   return (
     <div>
       <NavLink to={route}>
-        <button className="bg-[#19A463] text-base font-bold  p-3 rounded-lg text-white ] flex items-center justify-center">
+        <button type="submit" className="bg-[#19A463] text-base font-bold  p-3 rounded-lg text-white ] flex items-center justify-center">
           {seeAll}
           <span className="ml-2 text-xl">{icons}</span>
         </button>
--- src/Pages/Jobs/AddJobs.jsx
+++ src/Pages/Jobs/AddJobs.jsx
@@ -17,125 +17,109 @@ const AddJobs = () => {
         <div className="mx-auto max-w-7xl">
           <div className="bg-white shadow-xl rounded-xl px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
             <div className="-mx-3 md:flex mb-6">
-              <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+              <div className="md:w-full px-3 mb-6 md:mb-0">
                 <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
-                  id
+                  title
                 </label>
                 <input
                   className="appearance-none block w-full bg-yellow-50 text-grey-darker border border-red rounded-lg  py-3 px-4 mb-3"
                   id="grid-first-name"
                   type="text"
-                  placeholder="ID"
+                  placeholder="Ititle"
                 />
               </div>
-              <div className="md:w-1/2 px-3">
-                <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
-                  Way
-                </label>
-                <input
-                  className="appearance-none block w-full bg-yellow-50 text-grey-darker border border-grey-lighter rounded-lg py-3 px-4"
-                  id="grid-last-name"
-                  type="text"
-                  placeholder="Way"
-                />
-              </div>
+             
             </div>
             <div className="-mx-3 md:flex  mb-6">
-              <div className="md:w-full px-3">
-                <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
-                  logo
-                </label>
-                <input
-                  className="appearance-none block w-full bg-yellow-50 text-grey-darker border border-grey-lighter rounded-lg py-3 px-4 mb-3"
-                  id="grid-password"
-                  type="password"
-                  placeholder="logo"
-                />
-              </div>
               <div className="md:w-full px-3">
                 <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
                   location
                 </label>
                 <input
-                  className="appearance-none block w-full  bg-yellow-50 text-grey-darker border border-grey-lighter rounded-lg py-3 px-4 mb-3"
-                  id="grid-password"
-                  type="password"
-                  placeholder="location"
-                />
-              </div>
-            </div>
-            <div className="-mx-3 md:flex  mb-6">
-              <div className="md:w-full px-3">
-                <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
-                  logo
-                </label>
-                <input
                   className="appearance-none block w-full bg-yellow-50 text-grey-darker border border-grey-lighter rounded-lg py-3 px-4 mb-3"
                   id="grid-password"
                   type="password"
-                  placeholder="logo"
-                />
-              </div>
-              <div className="md:w-full px-3">
-                <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
-                  location
-                </label>
-                <input
-                  className="appearance-none block w-full  bg-yellow-50 text-grey-darker border border-grey-lighter rounded-lg py-3 px-4 mb-3"
-                  id="grid-password"
-                  type="password"
                   placeholder="location"
                 />
               </div>
-            </div>
-            <div className="-mx-3 md:flex  mb-6">
               <div className="md:w-full px-3">
                 <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
                   salary
                 </label>
                 <input
-                  className="appearance-none block w-full bg-yellow-50 text-grey-darker border border-grey-lighter rounded-lg py-3 px-4 mb-3"
+                  className="appearance-none block w-full  bg-yellow-50 text-grey-darker border border-grey-lighter rounded-lg py-3 px-4 mb-3"
                   id="grid-password"
                   type="password"
                   placeholder="salary"
                 />
               </div>
+            </div>
+            <div className="-mx-3 md:flex  mb-6">
               <div className="md:w-full px-3">
                 <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
                   position
                 </label>
                 <input
-                  className="appearance-none block w-full  bg-yellow-50 text-grey-darker border border-grey-lighter rounded-lg py-3 px-4 mb-3"
+                  className="appearance-none block w-full bg-yellow-50 text-grey-darker border border-grey-lighter rounded-lg py-3 px-4 mb-3"
                   id="grid-password"
                   type="password"
                   placeholder="position"
                 />
               </div>
-            </div>
-            <div className="-mx-3 md:flex mb-2">
-              <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+              <div className="md:w-full px-3">
                 <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
-                  title
+                  Remote or Onsite
                 </label>
                 <input
-                  className="appearance-none block w-full bg-yellow-50 text-grey-darker border border-grey-lighter rounded-lg py-3 px-4"
-                  id="grid-city"
-                  type="text"
-                  placeholder="title"
+                  className="appearance-none block w-full  bg-yellow-50 text-grey-darker border border-grey-lighter rounded-lg py-3 px-4 mb-3"
+                  id="grid-password"
+                  type="password"
+                  placeholder="Remote or Onsite"
                 />
               </div>
+            </div>
+            <div className="-mx-3 md:flex  mb-6">
+              <div className="md:w-full px-3">
+                <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
+                  Logo Link
+                </label>
+                <input
+                  className="appearance-none block w-full bg-yellow-50 text-grey-darker border border-grey-lighter rounded-lg py-3 px-4 mb-3"
+                  id="grid-password"
+                  type="password"
+                  placeholder="logo link"
+                />
+              </div>
+              <div className="md:w-full px-3">
+                <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
+                  Description
+                </label>
+                <textarea
+                  wrap="soft"
+                  rows="4"
+                  cols="4"
+                  className="appearance-none block w-full  bg-yellow-50 text-grey-darker border border-grey-lighter rounded-lg py-3 px-4 mb-3"
+                  id="grid-password"
+                  type="password"
+                  placeholder="Description"
+                />
+              </div>
+            </div>
+            <div className="-mx-3 md:flex mb-2">
+              
               <div className="md:w-1/2 px-3">
                 <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
-                  location
+                  Area
                 </label>
                 <div className="relative">
                   <select
                     className="block appearance-none w-full bg-yellow-50 border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded-lg"
                     id="grid-state"
                   >
-                    <option>New Mexico</option>
-                    <option>Missouri</option>
-                    <option>Texas</option>
+                    <option>bangladesh</option>
+                    <option>usa</option>
+                    <option>uk</option>
+                    <option>australia</option>
                   </select>
                 </div>
               </div>
@@ -151,7 +135,7 @@ const AddJobs = () => {
                 />
               </div>
             </div>
-            <div className="flex items-center justify-center my-8 ">
+            <div  className="flex items-center justify-center my-8 ">
               <Button seeAll="Submit" />
             </div>
           </div>
